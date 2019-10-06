d19c65f27f538693f4932de0eccda32a36370c82
[jkraut2] <[zevkraut@gmail.com]>
2019-10-06 16:41:20 -0400

    Update DiskWorker.java
    DiskWorker updates besides units class.

--- main/java/edu/touro/mco152/bm/DiskWorker.java
+++ main/java/edu/touro/mco152/bm/DiskWorker.java
@@ -1,6 +1,8 @@
 
 package edu.touro.mco152.bm;
 
+
+
 import static edu.touro.mco152.bm.App.KILOBYTE;
 import static edu.touro.mco152.bm.App.MEGABYTE;
 import static edu.touro.mco152.bm.App.blockSizeKb;
@@ -60,173 +62,22 @@ public class DiskWorker extends SwingWorker <Boolean, DiskMark> {
         }
    
         DiskMark wMark, rMark;
-        
+        checkResetData();
         Gui.updateLegend();
-        
-        if (App.autoReset == true) {
-            App.resetTestData();
-            Gui.resetTestData();
-        }
+
+
         
         int startFileNum = App.nextMarkNumber;
         
         if(App.writeTest) {
-            DiskRun run = new DiskRun(DiskRun.IOMode.WRITE, App.blockSequence);
-            run.setNumMarks(App.numOfMarks);
-            run.setNumBlocks(App.numOfBlocks);
-            run.setBlockSize(App.blockSizeKb);
-            run.setTxSize(App.targetTxSizeKb());
-            run.setDiskInfo(Util.getDiskInfo(dataDir));
-            
-            msg("disk info: ("+ run.getDiskInfo()+")");
-            
-            Gui.chartPanel.getChart().getTitle().setVisible(true);
-            Gui.chartPanel.getChart().getTitle().setText(run.getDiskInfo());
-            
-            if (App.multiFile == false) {
-                testFile = new File(dataDir.getAbsolutePath()+File.separator+"testdata.jdm");
-            }            
-            for (int m=startFileNum; m<startFileNum+App.numOfMarks && !isCancelled(); m++) {
-                
-                if (App.multiFile == true) {
-                    testFile = new File(dataDir.getAbsolutePath()
-                            + File.separator+"testdata"+m+".jdm");
-                }   
-                wMark = new DiskMark(WRITE);
-                wMark.setMarkNum(m);
-                long startTime = System.nanoTime();
-                long totalBytesWrittenInMark = 0;
-
-                String mode = "rw";
-                if (App.writeSyncEnable) { mode = "rwd"; }
-                
-                try {
-                    try (RandomAccessFile rAccFile = new RandomAccessFile(testFile,mode)) {
-                        for (int b=0; b<numOfBlocks; b++) {
-                            if (App.blockSequence == DiskRun.BlockSequence.RANDOM) {
-                                int rLoc = Util.randInt(0, numOfBlocks-1);
-                                rAccFile.seek(rLoc*blockSize);
-                            } else {
-                                rAccFile.seek(b*blockSize);
-                            }
-                            rAccFile.write(blockArr, 0, blockSize);
-                            totalBytesWrittenInMark += blockSize;
-                            wUnitsComplete++;
-                            unitsComplete = rUnitsComplete + wUnitsComplete;
-                            percentComplete = (float)unitsComplete/(float)unitsTotal * 100f;
-                            setProgress((int)percentComplete);
-                        }
-                    }
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                long endTime = System.nanoTime();
-                long elapsedTimeNs = endTime - startTime;
-                double sec = (double)elapsedTimeNs / (double)1000000000;
-                double mbWritten = (double)totalBytesWrittenInMark / (double)MEGABYTE;
-                wMark.setBwMbSec(mbWritten / sec);
-                msg("m:"+m+" write IO is "+wMark.getBwMbSecAsString()+" MB/s     "
-                        + "("+Util.displayString(mbWritten)+ "MB written in "
-                        + Util.displayString(sec)+" sec)");
-                App.updateMetrics(wMark);
-                publish(wMark);
-                
-                run.setRunMax(wMark.getCumMax());
-                run.setRunMin(wMark.getCumMin());
-                run.setRunAvg(wMark.getCumAvg());
-                run.setEndTime(new Date());
+                writeTest();
             }
-            
-            EntityManager em = EM.getEntityManager();
-            em.getTransaction().begin();
-            em.persist(run);
-            em.getTransaction().commit();
-            
-            Gui.runPanel.addRun(run);
+
         }
         
-        
-        // try renaming all files to clear catch
-        if (App.readTest && App.writeTest && !isCancelled()) {
-            JOptionPane.showMessageDialog(Gui.mainFrame, 
-                "For valid READ measurements please clear the disk cache by\n" +
-                "using the included RAMMap.exe or flushmem.exe utilities.\n" +
-                "Removable drives can be disconnected and reconnected.\n" +
-                "For system drives use the WRITE and READ operations \n" +
-                "independantly by doing a cold reboot after the WRITE",
-                "Clear Disk Cache Now",JOptionPane.PLAIN_MESSAGE);
-        }
         
         if (App.readTest) {
-            DiskRun run = new DiskRun(DiskRun.IOMode.READ, App.blockSequence);
-            run.setNumMarks(App.numOfMarks);
-            run.setNumBlocks(App.numOfBlocks);
-            run.setBlockSize(App.blockSizeKb);
-            run.setTxSize(App.targetTxSizeKb());
-            run.setDiskInfo(Util.getDiskInfo(dataDir));
-              
-            msg("disk info: ("+ run.getDiskInfo()+")");
-            
-            Gui.chartPanel.getChart().getTitle().setVisible(true);
-            Gui.chartPanel.getChart().getTitle().setText(run.getDiskInfo());
-            
-            for (int m=startFileNum; m<startFileNum+App.numOfMarks && !isCancelled(); m++) {
-                
-                if (App.multiFile == true) {
-                    testFile = new File(dataDir.getAbsolutePath()
-                            + File.separator+"testdata"+m+".jdm");
-                }
-                rMark = new DiskMark(READ);
-                rMark.setMarkNum(m);
-                long startTime = System.nanoTime();
-                long totalBytesReadInMark = 0;
-
-                try {
-                    try (RandomAccessFile rAccFile = new RandomAccessFile(testFile,"r")) {
-                        for (int b=0; b<numOfBlocks; b++) {
-                            if (App.blockSequence == DiskRun.BlockSequence.RANDOM) {
-                                int rLoc = Util.randInt(0, numOfBlocks-1);
-                                rAccFile.seek(rLoc*blockSize);
-                            } else {
-                                rAccFile.seek(b*blockSize);
-                            }
-                            rAccFile.readFully(blockArr, 0, blockSize);
-                            totalBytesReadInMark += blockSize;
-                            rUnitsComplete++;
-                            unitsComplete = rUnitsComplete + wUnitsComplete;
-                            percentComplete = (float)unitsComplete/(float)unitsTotal * 100f;
-                            setProgress((int)percentComplete);
-                        }
-                    }
-                } catch (FileNotFoundException ex) {
-                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (IOException ex) {
-                    Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
-                }
-                long endTime = System.nanoTime();
-                long elapsedTimeNs = endTime - startTime;
-                double sec = (double)elapsedTimeNs / (double)1000000000;
-                double mbRead = (double) totalBytesReadInMark / (double) MEGABYTE;
-                rMark.setBwMbSec(mbRead / sec);
-                msg("m:"+m+" READ IO is "+rMark.getBwMbSec()+" MB/s    "
-                        + "(MBread "+mbRead+" in "+sec+" sec)");
-                App.updateMetrics(rMark);
-                publish(rMark);
-                
-                run.setRunMax(rMark.getCumMax());
-                run.setRunMin(rMark.getCumMin());
-                run.setRunAvg(rMark.getCumAvg());
-                run.setEndTime(new Date());
-            }
-            
-            EntityManager em = EM.getEntityManager();
-            em.getTransaction().begin();
-            em.persist(run);
-            em.getTransaction().commit();
-            
-            Gui.runPanel.addRun(run);
+            readTest(); 
         }
         App.nextMarkNumber += App.numOfMarks;      
         return true;
@@ -251,4 +102,179 @@ public class DiskWorker extends SwingWorker <Boolean, DiskMark> {
         App.state = App.State.IDLE_STATE;
         Gui.mainFrame.adjustSensitivity();
     }
-}
+
+    public void writeTest() {
+        DiskRun run = new DiskRun(DiskRun.IOMode.WRITE, App.blockSequence);
+        run.setNumMarks(App.numOfMarks);
+        run.setNumBlocks(App.numOfBlocks);
+        run.setBlockSize(App.blockSizeKb);
+        run.setTxSize(App.targetTxSizeKb());
+        run.setDiskInfo(Util.getDiskInfo(dataDir));
+
+        msg("disk info: (" + run.getDiskInfo() + ")");
+
+        Gui.chartPanel.getChart().getTitle().setVisible(true);
+        Gui.chartPanel.getChart().getTitle().setText(run.getDiskInfo());
+
+        if (App.multiFile == false) {
+            testFile = new File(dataDir.getAbsolutePath() + File.separator + "testdata.jdm");
+        }
+        for (int m = startFileNum; m < startFileNum + App.numOfMarks && !isCancelled(); m++) {
+
+            if (App.multiFile == true) {
+                testFile = new File(dataDir.getAbsolutePath()
+                        + File.separator + "testdata" + m + ".jdm");
+            }
+            wMark = new DiskMark(WRITE);
+            measureData(wMark,m);
+            App.updateMetrics(wMark);
+            publish(wMark);
+
+            setRunVars();}
+            persistData();
+            Gui.runPanel.addRun(run);
+
+
+    }
+
+    public void readTest(){
+        DiskRun run = new DiskRun(DiskRun.IOMode.READ, App.blockSequence);
+        setRunParams();
+        
+        
+
+        msg("disk info: ("+ run.getDiskInfo()+")");
+
+        Gui.chartPanel.getChart().getTitle().setVisible(true);
+        Gui.chartPanel.getChart().getTitle().setText(run.getDiskInfo());
+
+        for (int m=startFileNum; m<startFileNum+App.numOfMarks && !isCancelled(); m++) {
+
+            if (App.multiFile == true) {
+                testFile = new File(dataDir.getAbsolutePath()
+                        + File.separator+"testdata"+m+".jdm");
+            }
+            rMark = new DiskMark(READ);
+            measureData(rMark,m);
+            
+            App.updateMetrics(rMark);
+            publish(rMark);
+            setRunVars();
+
+
+        }
+
+        persistData();
+        Gui.runPanel.addRun(run);
+    }
+
+
+    public void checkResetData(){
+        if (App.autoReset == true) {
+            App.resetTestData();
+            Gui.resetTestData();
+        }
+    }
+
+        public void persistData() {
+            EntityManager em = EM.getEntityManager();
+            em.getTransaction().begin();
+            em.persist(run);
+            em.getTransaction().commit();
+        }
+
+        public void setRunParams(){
+        run.setNumMarks(App.numOfMarks);
+        run.setNumBlocks(App.numOfBlocks);
+        run.setBlockSize(App.blockSizeKb);
+        run.setTxSize(App.targetTxSizeKb());
+        run.setDiskInfo(Util.getDiskInfo(dataDir));
+        }
+
+        public void setRunVars(){
+            run.setRunMax(rMark.getCumMax());
+            run.setRunMin(rMark.getCumMin());
+            run.setRunAvg(rMark.getCumAvg());
+            run.setEndTime(new Date());
+        }
+        measureData(DiskMark d, int m){
+            if(d.type == DiskMark.MarkType.WRITE){
+                d.setMarkNum(m);
+            long startTime = System.nanoTime();
+            long totalBytesWrittenInMark = 0;
+
+            String mode = "rw";
+            if (App.writeSyncEnable) {
+                mode = "rwd";
+            }
+
+            try {
+                try (RandomAccessFile rAccFile = new RandomAccessFile(testFile, mode)) {
+                    for (int b = 0; b < numOfBlocks; b++) {
+                        if (App.blockSequence == DiskRun.BlockSequence.RANDOM) {
+                            int rLoc = Util.randInt(0, numOfBlocks - 1);
+                            rAccFile.seek(rLoc * blockSize);
+                        } else {
+                            rAccFile.seek(b * blockSize);
+                        }
+                        rAccFile.write(blockArr, 0, blockSize);
+                        totalBytesWrittenInMark += blockSize;
+                        wUnitsComplete++;
+                        unitsComplete = rUnitsComplete + wUnitsComplete;
+                        percentComplete = (float) unitsComplete / (float) unitsTotal * 100f;
+                        setProgress((int) percentComplete);
+                    }
+                }
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            long endTime = System.nanoTime();
+            long elapsedTimeNs = endTime - startTime;
+            double sec = (double) elapsedTimeNs / (double) 1000000000;
+            double mbWritten = (double) totalBytesWrittenInMark / (double) MEGABYTE;
+            wMark.setBwMbSec(mbWritten / sec);
+            msg("m:" + m + " write IO is " + d.getBwMbSecAsString() + " MB/s     "
+                    + "(" + Util.displayString(mbWritten) + "MB written in "
+                    + Util.displayString(sec) + " sec)");
+            }
+            else if(d.type == DiskMark.MarkType.READ)
+            {
+                rMark.setMarkNum(m);
+            long startTime = System.nanoTime();
+            long totalBytesReadInMark = 0;
+
+            try {
+                try (RandomAccessFile rAccFile = new RandomAccessFile(testFile,"r")) {
+                    for (int b=0; b<numOfBlocks; b++) {
+                        if (App.blockSequence == DiskRun.BlockSequence.RANDOM) {
+                            int rLoc = Util.randInt(0, numOfBlocks-1);
+                            rAccFile.seek(rLoc*blockSize);
+                        } else {
+                            rAccFile.seek(b*blockSize);
+                        }
+                        rAccFile.readFully(blockArr, 0, blockSize);
+                        totalBytesReadInMark += blockSize;
+                        rUnitsComplete++;
+                        unitsComplete = rUnitsComplete + wUnitsComplete;
+                        percentComplete = (float)unitsComplete/(float)unitsTotal * 100f;
+                        setProgress((int)percentComplete);
+                    }
+                }
+            } catch (FileNotFoundException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(App.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            long endTime = System.nanoTime();
+            long elapsedTimeNs = endTime - startTime;
+            double sec = (double)elapsedTimeNs / (double)1000000000;
+            double mbRead = (double) totalBytesReadInMark / (double) MEGABYTE;
+            rMark.setBwMbSec(mbRead / sec);
+            msg("m:"+m+" READ IO is "+rMark.getBwMbSec()+" MB/s    "
+                    + "(MBread "+mbRead+" in "+sec+" sec)");
+            }
+            
+        }
+    }   
